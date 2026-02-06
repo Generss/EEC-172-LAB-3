eec98898642598c145937a031ad260e6a7a14861
Josh <joshaclark@gmail.com>
2026-02-06 14:21:08 -0800

    initial commit

---
+++ spi_demo/Debug/Adafruit_SSD1351.d
@@ -0,0 +1,69 @@
+# FIXED
+
+Adafruit_SSD1351.obj: ../Adafruit_SSD1351.c
+Adafruit_SSD1351.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h
+Adafruit_SSD1351.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+Adafruit_SSD1351.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+Adafruit_SSD1351.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+Adafruit_SSD1351.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h
+Adafruit_SSD1351.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_types.h
+Adafruit_SSD1351.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_memmap.h
+Adafruit_SSD1351.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_common_reg.h
+Adafruit_SSD1351.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_ints.h
+Adafruit_SSD1351.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/gpio.h
+Adafruit_SSD1351.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/spi.h
+Adafruit_SSD1351.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom.h
+Adafruit_SSD1351.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_map.h
+Adafruit_SSD1351.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_patch.h
+Adafruit_SSD1351.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/utils.h
+Adafruit_SSD1351.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/prcm.h
+Adafruit_SSD1351.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/uart.h
+Adafruit_SSD1351.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/interrupt.h
+Adafruit_SSD1351.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/example/common/uart_if.h
+Adafruit_SSD1351.obj: ../pin_mux_config.h
+Adafruit_SSD1351.obj: ../Adafruit_SSD1351.h
+
+../Adafruit_SSD1351.c:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h:
+
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_types.h:
+
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_memmap.h:
+
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_common_reg.h:
+
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_ints.h:
+
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/gpio.h:
+
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/spi.h:
+
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom.h:
+
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_map.h:
+
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_patch.h:
+
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/utils.h:
+
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/prcm.h:
+
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/uart.h:
+
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/interrupt.h:
+
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/example/common/uart_if.h:
+
+../pin_mux_config.h:
+
+../Adafruit_SSD1351.h:
+
